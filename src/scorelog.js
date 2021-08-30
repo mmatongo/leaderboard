a9e870d33d356505414a129ad4f2e3cbbbae5bc6
Linux User <m_matongo@icloud.com>
2021-08-30 12:26:00 +0000

    add scorelog.js to src folder

---
+++ src/scorelog.js
@@ -0,0 +1,12 @@
+const scores = (scorelog) => {
+  const displayScores = document.getElementById('scores');
+
+  scorelog.forEach(
+    (entry) =>
+      displayScores.insertAdjacentHTML(
+        "beforeend"
+      )`<div>${entry.name}: ${entry.score}</div>`
+  );
+};
+
+export { scores };
