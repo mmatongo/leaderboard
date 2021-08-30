d4bcef91b6703f11410f36b99637ef15c7af8b2f
Linux User <m_matongo@icloud.com>
2021-08-30 12:25:39 +0000

    add index.js in src folder

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { scores } from './scorelog';
+
+const scorelog = [
+  {
+    name: 'Name',
+    score: 100,
+  },
+  {
+    name: 'Name',
+    score: 20,
+  },
+  {
+    name: 'Name',
+    score: 50,
+  },
+  {
+    name: 'Name',
+    score: 78,
+  },
+  {
+    name: 'Name',
+    score: 125,
+  },
+  {
+    name: 'Name',
+    score: 77,
+  },
+  {
+    name: 'Name',
+    score: 42,
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => scores(scorelog));
